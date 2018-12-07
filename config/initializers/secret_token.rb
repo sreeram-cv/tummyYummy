4516defb4a27396a5a9ace4a5921cd38ba5804fe
sreeram.v <sreeram.venkitaraman@freshworks.com>
2018-12-07 14:33:46 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HappyTummy::Application.config.secret_key_base = '7d36b1c993dc5ca2ec69cadcdbf779d0a32b2e6c451708861ab79c347c777c6e4b06833c60f892eff41014c9ed73f739e50b626ba3650aca02697c0f5880a25a'
